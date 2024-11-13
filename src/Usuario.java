3af783784424c25b8a159a819ed32b15ed88af71
Efraín Ancán <efrain.ancan@gmail.com>
2024-11-13 00:37:20 -0300

    cambios v2

--- src/Usuario.java
+++ src/Usuario.java
@@ -27,19 +27,29 @@ public abstract class Usuario {
     }
     /*
     //mejorar
-    public void ValidarRun(){
-        int contador;
-        contador=2;
-        Acumulador=0;
-        while (RUN!=0){
-            Multiplo=(RUN%10)*contador;
-            Acumulador=Acumulador+Multiplo;
-            RUN=RUN/10;
-            contador=contador++;
-            if(contador==8){
-                contador=2;
+    public static boolean validarRut(String rut) {
+
+        boolean validacion = false;
+        try {
+            rut =  rut.toUpperCase();
+            rut = rut.replace(".", "");
+            rut = rut.replace("-", "");
+            int rutAux = Integer.parseInt(rut.substring(0, rut.length() - 1));
+
+            char dv = rut.charAt(rut.length() - 1);
+
+            int m = 0, s = 1;
+            for (; rutAux != 0; rutAux /= 10) {
+                s = (s + rutAux % 10 * (9 - m++ % 6)) % 11;
             }
+            if (dv == (char) (s != 0 ? s + 47 : 75)) {
+                validacion = true;
+            }
+
+        } catch (java.lang.NumberFormatException e) {
+        } catch (Exception e) {
         }
+        return validacion;
     }
     */
     public String getNombreCompleto() {
